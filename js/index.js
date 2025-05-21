95cc6f5fb1c4e2bcee23390a7592a39609745673
azizmahmud <azizmahmud1000@gmail.com>
2025-05-22 05:03:33 +0600

    Course Section Added

--- js/index.js
+++ js/index.js
@@ -11,3 +11,31 @@ close.addEventListener("click", () => {
   menu.classList.add("translate-x-full", "opacity-0");
   menu.classList.remove("translate-x-0", "opacity-100");
 });
+
+// Swiper Slider
+
+var swiper = new Swiper(".course-slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".course-button-next",
+    prevEl: ".course-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1.3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
